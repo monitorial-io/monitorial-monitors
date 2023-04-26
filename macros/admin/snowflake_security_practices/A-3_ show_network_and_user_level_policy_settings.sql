870e60e405aa35185d5b8253e7994438e2664d2e
Simon Peck <simon@dataengineers.co.nz>
2023-04-26 14:03:32 +1200

    add d and e plus a3 with execute immediate test

--- macros/admin/snowflake_security_practices/A-3_ show_network_and_user_level_policy_settings.sql
+++ macros/admin/snowflake_security_practices/A-3_ show_network_and_user_level_policy_settings.sql
@@ -1,2 +1,9 @@
+{% macro has_network_policy () -%}
+--execute_immediate=show parameters like 'network_policy' in account
 
+select * 
+from 
+    table(result_scan(last_query_id()) 
+where "value" = ''
+{%- endmacro %}
 
