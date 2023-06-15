c4879a73e02e74e4e34169e03e65f43c94f98518
Simon Peck <simon@dataengineers.co.nz>
2023-06-15 19:24:58 +1200

    remove duplicate monitor

--- macros/security_administration/snowflake_security_practices/A1_verify_connections_logging_in_from_specific_networks/login_from_blocked_ip_address.sql
+++ macros/security_administration/snowflake_security_practices/A1_verify_connections_logging_in_from_specific_networks/login_from_blocked_ip_address.sql
@@ -12,34 +12,3 @@ where error_message = 'INCOMING_IP_BLOCKED'
 and event_timestamp >= dateadd(minutes, -{{ time_filter }}, current_time)
 order by event_timestamp desc
 {%- endmacro %}
-
-{% macro login_failures_by_ip_address (time_filter=1440) -%}
-select distinct 
-    client_ip, 
-    count(client_ip) as count_of_failured_logins
-from snowflake.account_usage.login_history
-where is_success='NO'
-and event_timestamp >= dateadd(minutes, -{{ time_filter }}, current_time)
-group by client_ip
-{%- endmacro %}
-
-{% macro most_blocked_ip_addresses (time_filter=1440) -%}
-select distinct 
-    client_ip as blocked_source_ip,
-    count (client_ip), 
-    user_name,
-    reported_client_type as driver,
-    first_authentication_factor as auth_type
-from 
-    snowflake.account_usage.login_history
-where error_message = 'INCOMING_IP_BLOCKED'
-and event_timestamp >= dateadd(minutes, -{{ time_filter }}, current_timestamp)
-group by 
-    user_name, 
-    client_ip, 
-    reported_client_type, 
-    first_authentication_factor
-order by 
-    count (client_ip) desc
-{%- endmacro %}
-
