c7d278a8545199c55303d77cdccbb8891333f2fe
Simon Peck <simon@dataengineers.co.nz>
2023-06-15 19:15:10 +1200

    Removed manual monitors

--- manual/admin/snowflake_security_checklist/failure_count_by_ip_address.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    client_ip,
-    count(client_ip) AS count_of_failured_logins
-FROM snowflake.account_usage.login_history
-WHERE is_success = 'NO'
-GROUP BY client_ip
--- manual/admin/snowflake_security_checklist/key_pair_with_password.sql
+++
@@ -1,5 +0,0 @@
-SELECT * 
-FROM snowflake.account_usage.users
-WHERE
-    has_rsa_public_key = 'true'
-    AND has_password = 'true'
