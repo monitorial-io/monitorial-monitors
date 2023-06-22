b0ba737cf40c28e6d1aa0a43812f35dadce0e723
Simon Peck <simon@dataengineers.co.nz>
2023-06-22 21:27:21 +1200

    add rbar_detection

--- macros/performance/rbar_detection.sql
+++ macros/performance/rbar_detection.sql
@@ -1,7 +1,18 @@
 {% macro rbar_detection(query_types = ["INSERT","UPDATE","DELETE","MERGE"], query_count_greater_than = 10, time_filter=1440) -%}
 
+with get_account_details as (
+    select
+        replace(split(split(data.value:host::string, '.')[0], '-')[0], '"', '') as organization_name,
+        replace(split(split(data.value:host::string, '.')[0], '-')[1], '"', '') as account_name
+    from table(flatten(select parse_json(system$allowlist()))) as data
+    where data.value:type::string in ('SNOWFLAKE_DEPLOYMENT_REGIONLESS')
+)
 select 
-    query_text, 
+    'app.snowflake.com/' || (select organization_name from get_account_details) || 
+    '/' || (select account_name from get_account_details) || 
+    '/#/compute/history/queries/' || 
+    max(query_id) || 
+    '/detail' as sample_query_profile,
     session_id, 
     user_name, 
     count(*) as query_count
