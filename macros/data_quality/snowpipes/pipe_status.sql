d9fa28652225db449f511fb53e5bbf3f0609f53a
jonhopper-dataengineers <jon@dataengineers.co.nz>
2023-07-21 09:30:14 +1200

    fixed schedules and print documentation

--- macros/data_quality/snowpipes/pipe_status.sql
+++ macros/data_quality/snowpipes/pipe_status.sql
@@ -1,6 +1,6 @@
-{% macro pipe_status(status_exception_list=["RUNNING", "PAUSED", "STOPPED_CLONED"], pipe) -%}
+{% macro pipe_status(pipe, pipe_status_exception_list=["RUNNING", "PAUSED", "STOPPED_CLONED"]) -%}
 select
     datediff(minute,parse_json(SYSTEM$PIPE_STATUS('{{ pipe }}')):lastIngestedTimestamp::timestamp,current_timestamp) as minutes_since_last_data,
     parse_json(SYSTEM$PIPE_STATUS('{{ pipe }}')):executionState::text as executionState
-where executionState not in ({%- for state in status_exception_list -%}'{{state}}'{% if not loop.last %},{% endif %}{% endfor %})
+where executionState not in ({%- for state in pipe_status_exception_list -%}'{{state}}'{% if not loop.last %},{% endif %}{% endfor %})
 {%- endmacro %}
