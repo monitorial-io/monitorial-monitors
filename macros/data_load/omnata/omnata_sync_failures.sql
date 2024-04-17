82ffe80396c5a54283d5cec35216104022cbbb3d
jonhopper-dataengineers <jon@dataengineers.co.nz>
2024-04-17 17:13:41 +1200

    updated location of monitors

---
+++ macros/data_load/omnata/omnata_sync_failures.sql
@@ -0,0 +1,15 @@
+{% macro omnata_sync_failures(minutes=30) -%}
+
+select
+    sync_run.sync_id
+    , sync.sync_name
+    , sync_run.run_start_datetime
+    , sync_run.health_state
+    , sync_run.inbound_global_error_by_stream
+from omnata_sync_engine.data_views.sync_run
+inner join omnata_sync_engine.data_views.sync on sync.sync_id = sync_run.sync_id
+where lower(sync_run.health_state) = 'failed'
+and sync_run.run_start_datetime > dateadd(minute, -{{ minutes }}, current_timestamp())
+and lower(sync.health_state) != 'healthy'
+
+{%- endmacro %}
