f4d00890c4b0e1e00952e14e5a7919276cdd7348
Simon Peck <simon@dataengineers.co.nz>
2024-06-04 15:21:20 +1200

    3 new monitors (#12)
    * 3 new monitors
    
    * time filter for ip address

---
+++ macros/security_administration/security_monitors/login_attempts_suspect_clients.sql
@@ -0,0 +1,17 @@
+{% macro login_suspect_clients (time_filter=1440) -%}
+select
+    *
+from
+    snowflake.account_usage.sessions
+where
+    parse_json(client_environment):application = 'rapeflake'
+    or
+    (
+        parse_json(client_environment):application = 'dbeaver_dbeaverultimate'
+        and
+        parse_json(client_environment):os = 'windows server 2022'
+    )
+    and 
+        created_on > dateadd(minutes, -{{ time_filter }}, current_timestamp)
+order by created_on
+{%- endmacro %}
