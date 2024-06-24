757a56c5b99d52090dfb3b401ab8420f2f95a8bd
Simon Peck <simon@dataengineers.co.nz>
2024-06-24 19:07:48 +1200

    trust center monitors

---
+++ macros/security_administration/trust_center/cis_benchmarks_cis1_11.sql
@@ -0,0 +1,17 @@
+{% macro cis_benchmarks_cis1_11() -%}
+select
+  scanner_id,
+  end_timestamp as last_run_timestamp,
+  case 
+    when critical_risk_count > 0 then 'critical'
+    when high_risk_count > 0 then 'high'
+    when medium_risk_count > 0 then 'medium'
+    when low_risk_count > 0 then 'low'
+  end as severity
+from
+  snowflake.trust_center.time_series_daily_findings
+where
+  scanner_id = 'CIS_BENCHMARKS_CIS1_11' and 
+  none_risk_count != 1 and
+  (critical_risk_count + high_risk_count + medium_risk_count + low_risk_count) > 0
+{%- endmacro %}
