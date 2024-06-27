28d36f11baaba6d071f640cc242c57e1a0c9973b
jonhopper-dataengineers <jon@dataengineers.co.nz>
2024-06-27 12:52:43 +1200

    updated outputs

--- macros/trust_center/center_for_internet_security_-_2_-_monitoring_and_alerting/monitoring_accountadmin_securityadmin_role_grants.sql
+++ macros/trust_center/center_for_internet_security_-_2_-_monitoring_and_alerting/monitoring_accountadmin_securityadmin_role_grants.sql
@@ -11,14 +11,16 @@ with scanner_entities as (
 )
 
 select
+    snowflake.trust_center.findings.scanner_name,
     snowflake.trust_center.time_series_daily_findings.scanner_id,
     snowflake.trust_center.time_series_daily_findings.end_timestamp
-        as last_run_timestamp,
+        as last_scan_time,
     snowflake.trust_center.findings.total_at_risk_count,
     snowflake.trust_center.findings.scanner_type,
     snowflake.trust_center.findings.suggested_action,
     snowflake.trust_center.findings.impact,
-    scanner_entities.entity_names
+    snowflake.trust_center.findings.severity,
+    ARRAY_TO_STRING(scanner_entities.entity_names, ', ') as entity_names
 from
     snowflake.trust_center.time_series_daily_findings
 inner join snowflake.trust_center.findings
